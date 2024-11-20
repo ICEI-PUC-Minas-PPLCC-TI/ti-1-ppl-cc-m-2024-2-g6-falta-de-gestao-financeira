161b2a680d8d4a6863f734e5da8c45fe6c3852ab
Adriano Araujo <adrianoaraujodsantos@hotmail.com>
2024-11-19 21:09:45 -0300

    Correção de bug na barra de pesquisa

--- codigo/public/assets/js/components/search-bar.js
+++ codigo/public/assets/js/components/search-bar.js
@@ -1,10 +1,12 @@
+import { formatStringForSearch } from "../lib/util.js";
 import { updateEntriesList } from "./entries-list.js";
+import entrie from "../controllers/entrie.js";
 
 export function searchBar() {
   const searchBar = document.getElementById("search");
 
   searchBar.addEventListener("blur", async (e) => {
-    const userEntries = await getAllUserEntries(user.id);
+    const userEntries = await entrie.getAllFromUser();
     const searchKey = formatStringForSearch(e.target.value);
 
     const filteredEntries = userEntries.filter((entrie) =>
