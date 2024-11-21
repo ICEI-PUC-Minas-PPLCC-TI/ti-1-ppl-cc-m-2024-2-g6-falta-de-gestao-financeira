90a531ace429bb98df051556e7344f4dfe11d5bd
Filipe Nery Rabelo <fineryrabelo@gmail.com>
2024-11-21 18:19:23 -0300

    Continuação do desenvolvimento

--- codigo/public/assets/js/pages/stocks.js
+++ codigo/public/assets/js/pages/stocks.js
@@ -1,4 +1,34 @@
+import { logoutButton } from "../components/logout-button.js";
+import { auth } from "../lib/auth.js";
+import { API_KEY } from "../lib/env.js";
+
+const user = auth();
+
+if (!user) {
+  window.location.replace("./login.html");
+}
+
+logoutButton();
+
 document.addEventListener("DOMContentLoaded", async () => {
+    const loggedInUser = auth();
+  if (!loggedInUser) {
+    window.location.replace("/login.html");
+    return;
+  }
+
+  const API_URL = `https://api.twelvedata.com/exchanges?type=stock&apikey=${API_KEY}`;
+  fetch(API_URL)
+    .then(res => {
+        if(!res.ok) {
+            throw new Error('Erro ao buscar dados da API');
+        }
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+    });
+
     const buttons = document.querySelectorAll('.btn'); // Seleciona todos os botões com a classe 'btn'
     
     buttons.forEach(button => {
@@ -10,5 +40,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             this.classList.add('selecionado');
         });
     });
+
+
     
 });
